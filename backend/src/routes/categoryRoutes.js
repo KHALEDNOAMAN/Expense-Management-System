9f88deed170353b8471247c456af383a27285df5
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 15:57:47 +0300

    Add backend/src/routes/categoryRoutes.js

---
+++ backend/src/routes/categoryRoutes.js
@@ -0,0 +1,2 @@
+﻿const express = require('express'); const r = express.Router(); const c = require('../controllers/categoryController');
+r.get('/', c.getAll); r.post('/', c.create); module.exports = r;
