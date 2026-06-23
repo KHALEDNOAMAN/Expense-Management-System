c6fc29a7cb717c07bc4689569bda88eeb4598a2b
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 15:57:28 +0300

    Add backend/migrations/003_department_budgets.js

---
+++ backend/migrations/003_department_budgets.js
@@ -0,0 +1,8 @@
+﻿exports.up = function(knex) {
+  return knex.schema.createTable('department_budgets', (t) => {
+    t.increments('id').primary(); t.integer('department_id').notNullable(); t.integer('fiscal_year').notNullable();
+    t.decimal('budget_amount', 15, 2).notNullable(); t.decimal('spent_amount', 15, 2).defaultTo(0);
+    t.decimal('remaining_amount', 15, 2); t.unique(['department_id', 'fiscal_year']); t.timestamps(true, true);
+  });
+};
+exports.down = (knex) => knex.schema.dropTable('department_budgets');
