05380d1d3c0b23ad2682ffd172a21b7389323e89
Khaled Noaman <81718367+KHALEDNOAMAN@users.noreply.github.com>
2026-06-23 15:57:36 +0300

    Add backend/src/controllers/budgetController.js

---
+++ backend/src/controllers/budgetController.js
@@ -0,0 +1,10 @@
+﻿const { query } = require('../config/database');
+class BudgetController {
+  static async getAll(req, res, next) { try { const r = await query('SELECT * FROM department_budgets ORDER BY fiscal_year DESC'); res.json({ success: true, data: r.rows }); } catch (e) { next(e); } }
+  static async set(req, res, next) {
+    try { const { departmentId, fiscalYear, budgetAmount } = req.body;
+      const r = await query(`INSERT INTO department_budgets (department_id, fiscal_year, budget_amount, remaining_amount) VALUES ($1,$2,$3,$3) ON CONFLICT (department_id, fiscal_year) DO UPDATE SET budget_amount = $3, remaining_amount = $3 - department_budgets.spent_amount RETURNING *`, [departmentId, fiscalYear, budgetAmount]);
+      res.json({ success: true, data: r.rows[0] });
+    } catch (e) { next(e); } }
+}
+module.exports = BudgetController;
